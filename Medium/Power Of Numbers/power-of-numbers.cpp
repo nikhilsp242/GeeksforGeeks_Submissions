534ace74344d0b069c7f861722c4ba8770a1ff97
Nikhil S P <112267674+nikhilsp242@users.noreply.github.com>
2023-07-12 09:23:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Power Of Numbers/power-of-numbers.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+# define mod 1000000007
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //You need to complete this fucntion
+    
+    long long power(int N,int R)
+    {
+       //Your code here
+       /*                                                //O(R)
+        long long num = (long long)pow(N,R) % mod;
+        return num;
+        */
+        if(R==0)
+            return 1;
+        long long result = 1;
+        long long base = N;
+        while(R>0){
+            if(R%2==1)
+                result = (result * base) % mod;
+            base = (base * base) % mod;
+            R /= 2;
+        }
+        return result;
+    }    
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
+    }
+}
+// } Driver Code Ends
