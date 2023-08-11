0531c03499481d0816ebcde7018c8f40eafd1500
Nikhil S P <112267674+nikhilsp242@users.noreply.github.com>
2023-08-11 19:49:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int low = 0;
+        int high = n-1;
+        
+        for(int i=0;i<=high;){
+            if(a[i] == 0) swap(a[i++],a[low++]);
+            else if(a[i]==2) swap(a[i],a[high--]);
+            else i++;
+        }
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
