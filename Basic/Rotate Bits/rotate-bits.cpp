9887cf02f2de14f80dcfd77ac03fe34e1a75bec0
Nikhil S P <112267674+nikhilsp242@users.noreply.github.com>
2023-09-20 20:00:32 +0530

    Update rotate-bits.cpp

--- Basic/Rotate Bits/rotate-bits.cpp
+++ Basic/Rotate Bits/rotate-bits.cpp
@@ -9,11 +9,10 @@ class Solution
         vector <int> rotate (int n, int d)
         {
             //code here.
-            vector <int> res(2,0);
             d %= 16;
-            res[0] = ((n << d) | (n >> (16 - d))) & 0xFFFF;
-            res[1] = ((n >> d) | (n << (16 - d))) & 0xFFFF;
-            return res;
+            int left = ((n << d) | (n >> (16 - d))) & 0xFFFF;
+            int right = ((n >> d) | (n << (16 - d))) & 0xFFFF;
+            return {left,right};
         }
 };
 
