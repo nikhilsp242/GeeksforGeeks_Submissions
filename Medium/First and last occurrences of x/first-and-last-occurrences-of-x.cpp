09ae9609dba799499a883270fa7df9ead9492b1d
Nikhil S P <112267674+nikhilsp242@users.noreply.github.com>
2023-09-22 14:11:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -10,17 +10,46 @@ class Solution
     {
         // code here
         int first = INT_MAX , last = INT_MIN;
-        bool found = false;
+        // bool found = false;
         
-        for(int i=0;i<n;i++){
-            if(arr[i] == x){
-                found = true;
-                first = min(first,i);
-                last = max(last,i);
+        // for(int i=0;i<n;i++){              //0.59
+        //     if(arr[i] == x){
+        //         found = true;
+        //         first = min(first,i);
+        //         last = max(last,i);
+        //     }
+        // }
+        
+        // int i=0;
+        // while(i<n && arr[i]<=x){            //0.45
+        //     if(arr[i]==x){
+        //         found = true;
+        //         first = min(first,i);
+        //         last = max(last,i);
+        //     }
+        //     i++;
+        // }
+        // if(!found) return {-1,-1};
+        // return {first,last};
+        
+        int left = 0, right = n-1;   
+        
+        while(left<=right){
+            int mid = left + (right - left) / 2;    //0.43
+            
+            if(arr[mid] == x){
+                first = min(first,mid);
+                last = max(last,mid);
+                
+                int i=mid-1,j=mid+1;
+                while(i>=0 && arr[i]==x) first = min(first,i--);
+                while(j<n && arr[j]==x) last = max(last,j++);
+                return {first,last};
             }
+            else if(arr[mid]<x) left = mid+1;
+            else right = mid-1;
         }
-        if(!found) return {-1,-1};
-        return {first,last};
+        return {-1,-1};
     }
 };
 
